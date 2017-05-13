de67ffbc1664da2c905dec105dc8e0de36eaf3e7
Mai <maigua.vzla@gmail.com>
2017-05-13 16:34:14 +0200

    price working, only left extras price

--- main.js
+++ main.js
@@ -37,14 +37,75 @@ let app = new Vue({
 			this.butType = true;
 		},
 		payment: function () {
-
-			for (item of this.coffee) {
-				console.log(item);
-				if (item.title == this.picked.type) {
-					console.log("inside if");
-				}
+			let price = 0;
+			if (this.picked.size === '' || this.picked.type === '') {
+				alert("please pick a size or type")
+			} else {
+				if (this.picked.size === 'Small') {
+					for (item of this.coffee) {
+						console.log(item);
+						if (item.title == this.picked.type) {
+							price = item.priceS;
+							console.log(" small")
+						}
+					} // end for s
+				}// end if small
+				if (this.picked.size === 'Medium') {
+					for (item of this.coffee) {
+						console.log(item);
+						if (item.title == this.picked.type) {
+							price = item.priceM;
+							console.log("medium");
+						}
+					} // end for M
+				}// end if M
+				if (this.picked.size === 'Large') {
+					for (item of this.coffee) {
+						console.log(item);
+						if (item.title == this.picked.type) {
+							price = item.priceL;
+							console.log(" L");
+						}
+					} // end for L
+				}// end if L
+			}// end else 
+			if (this.picked.extra === '') {
 			}
-		}
+			else {
+				if (this.picked.size === 'Small') {
+					for (item of this.extra) {
+						if (item.title == this.picked.type) {
+							price = price + item.priceS;
+							console.log("extra,  small", price)
+						}
+					} // end for s
+				}// end if small
+				if (this.picked.size === 'Medium') {
+					for (item of this.extra) {
+						console.log(item);
+						if (item.title == this.picked.type) {
+							price = price + item.priceM;
+							console.log("medium extra ", price);
+						}
+					} // end for M
+				}// end if M
+				if (this.picked.size === 'Large') {
+					for (item of this.extra) {
+						console.log(item);
+						if (item.title == this.picked.type) {
+							price = price.item.priceL;
+							console.log("L extra", price);
+						}
+					} // end for M
+				}// end if M
+
+			}// end else extra
+
+			this.total = price;
+
+		} // end funtion
+
+
 	}
 
 
